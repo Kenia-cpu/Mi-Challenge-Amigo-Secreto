1525f4f01fb0bc172f2b9a9af1e9ed9083a04f48
Kenia Rojas <rojaskenia951@gmail.com>
2025-08-17 18:59:21 -0400

    Primer commit: agregando archivos del proyecto

---
+++ app.js
@@ -0,0 +1,43 @@
+let amigos = [];
+
+const agregarAmigo = () => {
+  let nombre = document.getElementById("amigo");
+  if (nombre.value === "") {
+    nombre.focus();
+    snackbar("Debe ingresar un nombre");
+    return;
+  }
+  amigos.push(nombre.value);
+  mostrarAmigos();
+  document.getElementById("amigo").value = "";
+  nombre.focus();
+  console.log(nombre.value);
+};
+
+const mostrarAmigos = () => {
+  let amigosList = document.getElementById("listaAmigos");
+  amigosList.innerHTML = "";
+  amigos.forEach((amigo) => {
+    amigosList.innerHTML += `<li>${amigo}</li>`;
+  });
+};
+
+const sortearAmigo = () => {
+    if (amigos.length < 2) {
+        alert("Debe ingresar al menos dos amigos");
+        return;
+    }
+  let amigoSorteado = amigos[Math.floor(Math.random() * amigos.length)];
+  document.getElementById("resultado").innerHTML = amigoSorteado;
+};
+
+const snackbar = (mensaje) => {
+  let snackbarElement = document.getElementById("snackbar");
+  snackbarElement.className = "show";
+  snackbarElement.innerHTML = mensaje;
+  setTimeout(function () {
+    snackbarElement.className = snackbarElement.className.replace("show", "");
+  }, 3000);
+};
+
+ 
